2121728d1d27015927893d84e5d5e54b1f450385
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2024-10-27 15:42:00 +0000

    feat: print statement and the first file

---
+++ basic/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.print("Text without a new");
+        System.out.print("line in the end");
+        System.out.print("\n");
+        System.out.print(3+3);
+    }
+}
