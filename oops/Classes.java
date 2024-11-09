3c48d74237b237046c8330ea28886830ee9abc77
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2024-11-09 06:57:02 +0000

    static vs  public methods

---
+++ oops/Classes.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class Classes {
+    int x = 5;
+
+    public static void main(String[] args){
+        Classes klassen = new Classes();
+        System.out.println(klassen.x);
+    }
+}
