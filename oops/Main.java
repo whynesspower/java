42021bfe70c70d3005d8a43bccb836d167cdf74c
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2024-11-09 09:02:07 +0000

    package name importing funcitons in other classes

--- oops/Main.java
+++ oops/Main.java
@@ -1,10 +1,16 @@
 package oops;
+
 public class Main {
     int x = 5;
   
     public static void main(String[] args) {
       Main myObj = new Main();
       System.out.println(myObj.x);
+
+      StaticMethod myObj2 = new StaticMethod();
+      myObj2.second();
+      myObj2.first();
     }
+  
   }
   
